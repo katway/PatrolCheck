f91a92fe1e6974d8e89914274ee5bfc2669bbbad
John <katway@163.com>
2012-10-12 11:29:20 +0800

    添加主界面(初步模板)

--- Application/Program.cs
+++ Application/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace Application
+namespace WorkStation
 {
     static class Program
     {
@@ -15,7 +15,7 @@ namespace Application
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
