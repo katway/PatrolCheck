88e78ea2590291fa504ec332fa7fb13441a9e4a3
John <katway@163.com>
2012-11-01 14:18:11 +0800

    使用反射调用窗体

--- Application/Properties/AssemblyInfo.cs
+++ Application/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Application")]
+[assembly: AssemblyTitle("WorkStation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Application")]
+[assembly: AssemblyProduct("WorkStation")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Application/frmMain.cs
+++ Application/frmMain.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Reflection;
 
 namespace WorkStation
 {
@@ -408,6 +409,28 @@ namespace WorkStation
             frmAddItem item = new frmAddItem();
             item.Show();
         }
-      
+        private Dictionary<string, Form> FormMap = new Dictionary<string, Form>();
+        private void ShowForm(object sender, EventArgs e)
+        {
+
+            string className = (sender as ToolStripMenuItem).Name.Replace("tsmi", "frm");
+            MessageBox.Show(className);
+            if (!FormMap.Keys.Contains(className))
+            {
+                Assembly assem = Assembly.GetExecutingAssembly();
+                System.Type t = assem.GetType("WorkStation." + className);
+                object obj = null;
+                if (t != null)
+                    obj = Activator.CreateInstance(t);
+                if (obj != null)
+                {
+                    (obj as Form).MdiParent = this;
+                    FormMap.Add(className, obj as Form);
+                }
+            }
+            if (FormMap.Keys.Contains(className))
+                FormMap[className].Show();
+        }
+
     }
 }
